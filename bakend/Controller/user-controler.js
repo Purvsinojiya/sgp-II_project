dca89afa6242f67c49c4727916c764494f41d8b1
“poorv <you@example.com>
2023-07-09 10:58:30 +0530

    vvvvvvvvvvvv

--- bakend/Controller/user-controler.js
+++ bakend/Controller/user-controler.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
-const Singup = require('../model/Singup.js');
+const Signup = require('../model/Singup.js');
 const bcrypt = require('bcryptjs');
 const OTPGenerator = require('otp-generator');
+const twilio = require('twilio');
+
+const accountSid = 'AC73d85056055c0294033c3a3021c98f9e';
+const authToken = '59feeb0e2e45c16a6fc4ec698b86f4f7';
+const client = twilio(accountSid, authToken);
 
 const signup = async (req, res, next) => {
   console.log('Signup request received');
@@ -15,11 +20,21 @@ const signup = async (req, res, next) => {
   try {
     const otp = OTPGenerator.generate(6, { digits: true, alphabets: false, upperCase: false, specialChars: false });
 
-    // In place of sending OTP via SMS, you can implement your own logic here to deliver the OTP to the user.
-    // For example, you can send it via email, push notification, or any other preferred communication method.
+    // Send OTP via SMS using Twilio
+    const smsResponse = await client.messages.create({
+      body: `Your OTP is for Shivam EnterPrise: ${otp}`,
+  from: '+14178923736',
+        to: number
+    });
+
+    if (!smsResponse || smsResponse.errorCode) {
+      // Handle error if SMS sending fails
+      console.error('Error sending SMS:', smsResponse.errorCode);
+      return res.status(500).json({ message: 'Error sending OTP via SMS' });
+    }
 
     const hashedPassword = bcrypt.hashSync(password, 10);
-    const user = await Singup.create({ name, email, password: hashedPassword, number, otp });
+    const user = await Signup.create({ name, email, password: hashedPassword, number, otp });
 
     console.log('User created:', user);
     console.log('User OTP:', user.otp); // Display the user's OTP in the console
@@ -31,27 +46,23 @@ const signup = async (req, res, next) => {
   }
 };
 
-
-
 const verifyOTP = async (req, res, next) => {
   const { email, otp } = req.body;
- 
 
   if (!email || !otp) {
     return res.status(400).json({ message: 'Please provide email and OTP' });
   }
 
   try {
-    const user = await Singup.findOne({ email, otp });
+    const user = await Signup.findOne({ email, otp });
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
-    
-    console.log('User OTP:',user.otp);
-    console.log('Input OTP:', otp);
-  
 
-    if (user.otp !== otp) {  // Corrected the field name here from `user.otp` to `user.otpValue`
+    console.log('User OTP:', user.otp);
+    console.log('Input OTP:', otp);
+
+    if (user.otp !== otp) {
       return res.status(401).json({ message: 'Invalid OTP' });
     }
 
